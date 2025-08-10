9eaf8aa4acc288ee05f4c0167a9357ccfa18133c
Diky <890dikymaulidan@gmail.com>
2025-08-10 21:44:03 +0700

    chore(tailwind): add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 function App() {
     return (
         <>
-            <h1>Diky's Personal Site (Coming Soon)</h1>
+            <div className="flex justify-center items-center h-screen">
+                <h1 className="text-3xl text-indigo-400">
+                    Diky's Personal Site (Coming Soon)
+                </h1>
+            </div>
         </>
     );
 }
